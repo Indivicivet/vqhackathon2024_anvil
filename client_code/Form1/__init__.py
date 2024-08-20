79647a3315ac0eb4e6ff2f7a2e1c34248688e90b
indivicivet@gmail.com <indivicivet@gmail.com>
2024-08-20 10:51:52 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,3 +9,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def spin_motors_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call("run1s")

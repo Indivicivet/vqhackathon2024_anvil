188483b1bc5af20fa7140b5840777ddf099d4f63
indivicivet@gmail.com <indivicivet@gmail.com>
2024-08-20 18:13:32 +0000

    tidyup, WIP not yet implemented new slider

--- client_code/main/__init__.py
+++ client_code/main/__init__.py
@@ -5,41 +5,32 @@ from anvil_extras import augment
 
 class main(mainTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
   
     augment.set_event_handler(self.magic_text_box, "keydown", self.keydown_handler)
-    
+  
   def spin_motors_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("both", "f", t=1, s=self.spin_speed_slider.value)
 
   def spin_both_backward_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("both", "r", t=1, s=self.spin_speed_slider.value)
     
   def leftturn_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("left_turn", s=self.turn_sens_slider.value)
 
   def rightturn_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("right_turn", s=self.turn_sens_slider.value)
 
   def left_forward_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("left_only", "f", s=self.turn_sens_slider.value)
 
   def right_forward_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("right_only", "f", s=self.turn_sens_slider.value)
 
   def left_back_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("left_only", "r", s=self.turn_sens_slider.value)
 
   def right_back_click(self, **event_args):
-    """This method is called when the button is clicked"""
     anvil.server.call("right_only", "r", s=self.turn_sens_slider.value)
   
   def keydown_handler(self, **event_args):
@@ -47,9 +38,9 @@ class main(mainTemplate):
     key = event_args.get('key')
     print(key, key_code)
     if key == "w":
-      anvil.server.call("both", "f")
+      anvil.server.call("both", "f", s=self.spin_speed_slider.value)
     elif key == "s":
-      anvil.server.call("both", "r")
+      anvil.server.call("both", "r", s=self.spin_speed_slider.value)
     elif key == "q":
       anvil.server.call("left_only", "f", s=self.turn_sens_slider.value)
     elif key == "e":

816f33f07b84a4c55c2614299de1e19347550363
indivicivet@gmail.com <indivicivet@gmail.com>
2024-08-20 11:14:36 +0000

    leftright turn

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,3 +13,11 @@ class Form1(Form1Template):
   def spin_motors_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.server.call("run1s")
+
+  def leftturn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call("leftturn")
+
+  def rightturn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call("rightturn")
